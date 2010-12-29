85f96c55cf04ba19a6fcc2c98711e676f1b4ecb9
comex <comexk@gmail.com>
2010-12-29 20:27:19 +0100

    commit

---
+++ cs.py
@@ -0,0 +1,156 @@
+from construct import *
+import sys, os, re
+import plistlib
+
+class PlistAdapter(Adapter):
+    def _encode(self, obj, context):
+        return plistlib.writePlistToString(obj)
+    def _decode(self, obj, context):
+        return plistlib.readPlistFromString(obj)
+
+# talk about overdesign.
+
+# magic is in the blob struct
+
+Expr = LazyBound("expr", lambda: Expr_)
+Blob = LazyBound("blob", lambda: Blob_)
+
+CodeDirectory = Struct("CodeDirectory",
+    UBInt32("version"),
+    UBInt32("flags"),
+    UBInt32("hashOffset"),
+    UBInt32("identOffset"),
+    UBInt32("nSpecialSlots"),
+    UBInt32("nCodeSlots"),
+    UBInt32("codeLimit"),
+    UBInt8("hashSize"),
+    UBInt8("hashType"),
+    UBInt8("spare1"),
+    UBInt8("pageSize"),
+    UBInt32("spare2"),
+    If(lambda ctx: ctx['version'] >= 0x20100, UBInt32("scatterOffset")),
+)
+
+Data = Struct("Data",
+    UBInt32("length"),
+    Bytes("data", lambda ctx: ctx['length']),
+    Padding(lambda ctx: -ctx['length'] & 3),
+)
+
+CertSlot = Enum(UBInt32("slot"),
+    anchorCert = -1,
+    leafCert = 0,
+    _default_ = Pass,
+)
+
+Expr_ = Struct("Expr",
+    Enum(UBInt32("op"),
+        opFalse = 0,
+        opTrue = 1,
+        opIdent = 2,
+        opAppleAnchor = 3,
+        opAnchorHash = 4,
+        opInfoKeyValue = 5,
+        opAnd = 6,
+        opOr = 7,
+        opCDHash = 8,
+        opNot = 9,
+        opInfoKeyField = 10,
+        opCertField = 11,
+        opTrustedCert = 12,
+        opTrustedCerts = 13,
+        opCertGeneric = 14,
+        opAppleGenericAnchor = 15,
+        opEntitlementField = 16,
+    ),
+    Switch("data", lambda ctx: ctx['op'], {
+        'opIdent': Data,
+        'opAnchorHash': Sequence("AnchorHash", CertSlot, Data),
+        'opInfoKeyValue': Data,
+        'opAnd': Sequence("And", Expr, Expr),
+        'opOr': Sequence("Or", Expr, Expr),
+        'opNot': Expr,
+        'opCDHash': Data,
+        'opInfoKeyField': Data,
+        'opEntitlementField': Data,
+        'opCertField': CertSlot,
+        'opCertGeneric': CertSlot,
+        'opTrustedCert': CertSlot,
+    }, default = Pass),
+)
+
+
+Requirement = Struct("Requirement",
+    Const(UBInt32("kind"), 1),
+    Expr,
+)
+
+Entitlement = Struct("Entitlement",
+    # actually a plist
+    PlistAdapter(Bytes("data", lambda ctx: ctx['_']['length'] - 8)),
+)
+
+EntitlementsBlobIndex = Struct("BlobIndex",
+    Enum(UBInt32("type"),
+        kSecHostRequirementType = 1,
+        kSecGuestRequirementType = 2,
+        kSecDesignatedRequirementType = 3,
+        kSecLibraryRequirementType = 4,
+    ),
+    UBInt32("offset"),
+    Pointer(lambda ctx: ctx['_']['sb_start'] - 8 + ctx['offset'], Blob),
+)
+
+Entitlements = Struct("Entitlements", # actually a kind of super blob
+    Anchor("sb_start"),
+    UBInt32("count"),
+    StrictRepeater(lambda ctx: ctx['count'], EntitlementsBlobIndex),
+)
+
+BlobWrapper = Struct("BlobWrapper",
+    Bytes("data", lambda ctx: ctx['_']['length'] - 8),
+)
+
+Blob_ = Struct("Blob",
+    Anchor("blob_start"),
+    Enum(UBInt32("magic"),
+        CSMAGIC_REQUIREMENT = 0xfade0c00,
+        CSMAGIC_REQUIREMENTS = 0xfade0c01,
+        CSMAGIC_CODEDIRECTORY = 0xfade0c02,
+        CSMAGIC_ENTITLEMENT = 0xfade7171, # actually, this is kSecCodeMagicEntitlement, and not defined in the C version :psyduck:
+        CSMAGIC_BLOBWRAPPER = 0xfade0b01, # and this isn't even defined in libsecurity_codesigning; it's in _utilities
+    ),
+    UBInt32("length"),
+    Switch("data", lambda ctx: ctx['magic'], {
+        'CSMAGIC_REQUIREMENT': Requirement,
+        'CSMAGIC_REQUIREMENTS': Entitlements,
+        'CSMAGIC_CODEDIRECTORY': CodeDirectory,
+        'CSMAGIC_ENTITLEMENT': Entitlement,
+        'CSMAGIC_BLOBWRAPPER': BlobWrapper,
+    }),
+)
+
+BlobIndex = Struct("BlobIndex",
+    UBInt32("type"),
+    UBInt32("offset"),
+    Pointer(lambda ctx: ctx['_']['sb_start'] + ctx['offset'], Blob),
+)
+
+SuperBlob = Struct("SuperBlob",
+    Anchor("sb_start"),
+    Const(UBInt32("magic"), 0xfade0cc0),
+    UBInt32("length"),
+    UBInt32("count"),
+    StrictRepeater(lambda ctx: ctx['count'], BlobIndex),
+)
+
+# not going to bother to use construct for mach-o right now
+for blob in os.popen('otool -lvv "%s" | grep -A 3 LC_CODE_SIGNATURE' % sys.argv[1]).read().split('\n--\n'):
+    dataoff = int(re.search('dataoff\s*([0-9]+)', blob).group(1))
+    datasize = int(re.search('datasize\s*([0-9]+)', blob).group(1))
+    f = open(sys.argv[1])
+    f.seek(dataoff)
+    data = f.read(datasize)
+
+    parsed = SuperBlob.parse(data)
+    print parsed
