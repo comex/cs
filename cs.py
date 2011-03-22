079750b4a350ed97c09dc8da418784055ca2b726
comex <comexk@gmail.com>
2011-03-21 20:58:01 -0400

    add "dump certs" option

--- cs.py
+++ cs.py
@@ -7,6 +7,7 @@ from construct_try import construct_try
 if __name__ == '__main__':
     parser = OptionParser()
     parser.add_option('-H', '--hashes', dest='hashes', action='store_true', default=False, help='print the actual hashes')
+    parser.add_option('-c', '--certs', dest='certs', default=None, help='save the certificates blob (DER-encoded PKCS#7) to a file')
     options, args = parser.parse_args()
     filename = args[0]
 
@@ -15,4 +16,15 @@ if __name__ == '__main__':
 
     f = open(filename, 'rb')
     data = construct_try(lambda: macho.MachOOrFat.parse_stream(f))
-    print data
+    for cmd in data.data.commands:
+        if cmd.cmd == 'LC_CODE_SIGNATURE':
+            print cmd
+            if options.certs:
+                try:
+                    for blob in cmd.data.blob.data.BlobIndex:
+                        if blob.blob.magic == 'CSMAGIC_BLOBWRAPPER':
+                            open(options.certs, 'wb').write(blob.blob.data.data.value)
+                            break
+                except:
+                    pass
+
